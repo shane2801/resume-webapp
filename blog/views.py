486140ad3d74935c03fafe74b43258dd7534789d
shane <seannex7@gmail.com>
2020-08-30 13:26:48 +0100

    added  funtionalities

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from users.models import Profile
 
 def home(request):
     context = {
        'posts':  Post.objects.all()
     }
-    return render(request, 'blog/home.html',context)#context)
+    return render(request, 'blog/home.html',context)
+def resume(request):
+	context1 = {
+       'resumes':  Profile.objects.all()
+    }
+	return render(request, 'blog/resume.html',context1)
 
 
 	
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name = 'register'),
+    path('resume/', blog_views.resume, name="resume"),
     path('profile/', user_views.profile, name = 'profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name = 'logout'),
